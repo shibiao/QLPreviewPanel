52c21503fbaa444669877eb87de0b08ef4028cd2
WalkWithWind <956035125@qq.com>
2017-11-17 14:59:30 +0800

    QLPreviewPanel

--- QLPreviewPanel/ViewController.swift
+++ QLPreviewPanel/ViewController.swift
@@ -7,15 +7,56 @@
 //
 
 import Cocoa
+import Quartz
+class ViewController: NSViewController, QLPreviewPanelDelegate, QLPreviewPanelDataSource {
+    var sharedPanel: QLPreviewPanel!
+    func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
+        return 1
+    }
+    
+    func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
+        
+        if let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("1.jpg") {
+            print("___\(path)")
+            return path as QLPreviewItem
+        }
+        
+        return nil
+    }
+    
+    override func acceptsPreviewPanelControl(_ panel: QLPreviewPanel!) -> Bool {
+        return true
+    }
+    override func beginPreviewPanelControl(_ panel: QLPreviewPanel!) {
+        sharedPanel = panel
+        
+    }
+    override func endPreviewPanelControl(_ panel: QLPreviewPanel!) {
+        sharedPanel = nil
+    }
 
-class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        sharedPanel = QLPreviewPanel.shared()
+        
+        
+//        sharedPanel.contentView?.wantsLayer = true
+//        sharedPanel.contentView?.layer?.backgroundColor = NSColor.red.cgColor
+        
         // Do any additional setup after loading the view.
     }
+    @IBAction func handleTap(_ sender: Any) {
+        
+        sharedPanel.delegate = self
+        sharedPanel.dataSource = self
+        sharedPanel.makeKeyAndOrderFront(self)
+        sharedPanel.center()
 
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
